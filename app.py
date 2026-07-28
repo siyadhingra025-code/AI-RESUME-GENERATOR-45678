fbf8b7a52aa66a11baa2bf3aa5a036cb160e48a4
siyadhingra025-code <siyadhingra025@gmail.com>
2026-07-28 13:36:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ import base64
 OPTION = ["DELHI","NOIDA","GURGAON/GURUGRAM",
           'KANPUR', 'LUCKNOW', 'BANGLORE', 'PUNE']
 
-LOCATON = st.sidebar.multiselect("SELECT LOCATION: ',
+LOCATON = st.sidebar.multiselect("SELECT LOCATION: ",
                                    options = OPTIONS)
 JOB_PROFILE = ["PYTHON DEVELOPER",'GEN AI',
                  'FULL STACK DEVELOPER', 'DATA ANALYST']
