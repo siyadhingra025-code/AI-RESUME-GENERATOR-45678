7909f8a957301e2bbd302aa5fb4bd030015d1640
siyadhingra025-code <siyadhingra025@gmail.com>
2026-07-28 13:42:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -147,11 +147,11 @@ if st.button('generate resume'):
         code = code.replace("PROFILE_IMAGE_PLACEHOLDER", data_uri)
 
     
-    st.html(code , width="stretch" , unsafe_allow_javascript=True)
-    st.divider()
-    response = agent.invoke({'messages' :[{'role':'user','content':job_prompt}]})
-
-   job_code = response['messages'][-1].content[-1]['text']
-   st.html(job_code, width="stretch", unsafe_allow_javascript=True)
-                 
+        st.html(code , width="stretch" , unsafe_allow_javascript=True)
+        st.divider()
+        response = agent.invoke({'messages' :[{'role':'user','content':job_prompt}]})
+    
+       job_code = response['messages'][-1].content[-1]['text']
+       st.html(job_code, width="stretch", unsafe_allow_javascript=True)
+                     
 
